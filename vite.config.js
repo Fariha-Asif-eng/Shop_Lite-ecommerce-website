27e8226c2ae56d916a13b8642d809f82a97ba6ba
Fariha-Asif-eng <syedf4852@gmail.com>
2025-10-18 00:41:00 +0500

    changes in vite config file

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: "./", // ✅ This line is important for Vercel deployment
   plugins: [
     react(),
     tailwindcss(),
